8e6b66fe1a34dda3b1c8b8075a1c4fdb4d9eba2b
John Harrison <harrijk63@gmail.com>
2017-04-25 09:50:39 -0400

    break out marbles tasks into own functions

--- network_setup.sh
+++ network_setup.sh
@@ -6,18 +6,18 @@ CH_NAME=$2
 COMPOSE_FILE=docker-compose.yaml
 
 function printHelp () {
-	echo "Usage: ./network_setup <up|down> [ channel_name ]"
+  echo "Usage: ./network_setup <up|down> [ channel_name ]"
 }
 
 function validateArgs () {
-	if [ -z "${UP_DOWN}" ]; then
-		echo "Option up / down / restart not mentioned"
-		printHelp
-		exit 1
-	fi
-	if [ -z "${CH_NAME}" ]; then
-	  echo "setting to default channel 'mychannel'"
-	  CH_NAME=mychannel
+  if [ -z "${UP_DOWN}" ]; then
+    echo "Option up / down / restart not mentioned"
+    printHelp
+    exit 1
+  fi
+  if [ -z "${CH_NAME}" ]; then
+    echo "setting to default channel 'mychannel'"
+    CH_NAME=mychannel
         fi
 }
 
@@ -39,53 +39,76 @@ function removeUnwantedImages() {
         fi
 }
 
+function marblesSetup () {
+  cd marbles
+  npm install
+
+  # Update the marbles app credential files with the current channel name
+  ID='"channel_id": "'${CH_NAME}'",'
+  sed -i '35d' config/blockchain_creds1.json
+  sed -i '35d' config/blockchain_creds2.json
+  sed -i '35i\            '"${ID}" config/blockchain_creds1.json
+  sed -i '35i\            '"${ID}" config/blockchain_creds2.json
+}
+
+function marblesUIStart () {
+  echo -e "\nWaiting for marbles chaincode to initialize..."
+  sleep 30
+  echo "Starting the marbles UI application..."
+  cd marbles
+  gulp marbles1 > /dev/null 2>&1 &
+  gulp marbles2 > /dev/null 2>&1 &
+  echo ""
+  echo "Open two browser sessions:"
+  echo "1. <marbles-host-ip>:3001"
+  echo "2. <marbles-host-ip>:3002"
+  echo ""
+}
+
+function marblesCleanup () {
+  # Clear out marbles app hash
+  HASH='"last_startup_hash": ""'
+  sed -i '12d' marbles/config/marbles1.json
+  sed -i '12d' marbles/config/marbles2.json
+  sed -i '12i\    '"${HASH}" marbles/config/marbles1.json
+  sed -i '12i\    '"${HASH}" marbles/config/marbles2.json
+  pkill -f 'node app.js'
+}
+
 function networkUp () {
-	CURRENT_DIR=$PWD
-        source generateCfgTrx.sh $CH_NAME
-	cd marbles
-	npm install
-	cd $CURRENT_DIR
+  CURRENT_DIR=$PWD
+  source generateCfgTrx.sh $CH_NAME
+  marblesSetup
+  cd $CURRENT_DIR
 
-	# Update the marbles app credential files with the current channel name
-	ID='"channel_id": "'${CH_NAME}'",'
-	sed -i '35d' marbles/config/blockchain_creds1.json
-	sed -i '35d' marbles/config/blockchain_creds2.json
-	sed -i '35i\            '"${ID}" marbles/config/blockchain_creds1.json
-	sed -i '35i\            '"${ID}" marbles/config/blockchain_creds2.json
-
-	CHANNEL_NAME=$CH_NAME docker-compose -f $COMPOSE_FILE up -d 2>&1
-	if [ $? -ne 0 ]; then
-		echo "ERROR !!!! Unable to pull the images "
-		exit 1
-	fi
+  CHANNEL_NAME=$CH_NAME docker-compose -f $COMPOSE_FILE up -d 2>&1
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to pull the images "
+    exit 1
+  fi
+  marblesUIStart
 }
 
 function networkDown () {
+  marblesCleanup
   docker-compose -f $COMPOSE_FILE down
   #Cleanup the chaincode containers
-	clearContainers
-	#Cleanup images
-	removeUnwantedImages
-
-	# Clear out marbles app hash
-	HASH='"last_startup_hash": ""'
-	sed -i '12d' marbles/config/marbles1.json
-	sed -i '12d' marbles/config/marbles2.json
-	sed -i '12i\    '"${HASH}" marbles/config/marbles1.json
-	sed -i '12i\    '"${HASH}" marbles/config/marbles2.json
+  clearContainers
+  #Cleanup images
+  removeUnwantedImages
 }
 
 validateArgs
 
 #Create the network using docker compose
 if [ "${UP_DOWN}" == "up" ]; then
-	networkUp
+  networkUp
 elif [ "${UP_DOWN}" == "down" ]; then ## Clear the network
-	networkDown
+  networkDown
 elif [ "${UP_DOWN}" == "restart" ]; then ## Restart the network
-	networkDown
-	networkUp
+  networkDown
+  networkUp
 else
-	printHelp
-	exit 1
+  printHelp
+  exit 1
 fi
